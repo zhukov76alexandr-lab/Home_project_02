06653f5893416d69a5f1d7ce8e883a871515a213
Aleksandr <zhukov76alexandr@gmail.com>
2026-01-22 00:56:57 +0300

    Спрашиваем пользователя направление поездки

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 msc_price = 100_000
 spb_price = 80_000
 ekb_price = 70_000
+
+target = input("Введи направление (msc, spb, ekb): ")
+
+if target == "msc":
+    print(f"Стоимость поездки составит {msc_price}")
+elif target == "spb":
+    print(f"Стоимость поездки составит {spb_price}")
+elif target == "ekb":
+    print(f"Стоимость поездки составит {ekb_price}")
