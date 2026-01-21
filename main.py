e06841db828c6fca0d6801789be60b573d0501ca
Aleksandr <zhukov76alexandr@gmail.com>
2026-01-22 01:09:56 +0300

    Добавили Казань

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 msc_price = 100_000
 spb_price = 80_000
 ekb_price = 70_000
+kaz_price = 70_000
 
-target = input("Введи направление (msc, spb, ekb): ")
+target = input("Введи направление (msc, spb, ekb, kaz): ")
 
 if target == "msc":
     target_price = msc_price
@@ -10,5 +11,7 @@ elif target == "spb":
     target_price = spb_price
 elif target == "ekb":
     target_price = ekb_price
+elif target == "kaz":
+    target_price = kaz_price
 
 print(f"Стоимость поездки составит {target_price}. Приятного путешествия!")
