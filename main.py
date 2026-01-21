c599620694870bf5f7e663b6044559b087f93fe8
Aleksandr <zhukov76alexandr@gmail.com>
2026-01-22 01:32:09 +0300

    Добавил количество пользователей

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ spb_price = 80_000
 ekb_price = 70_000
 
 target = input("Введи направление (msc, spb, ekb): ")
+count = int(input("Сколько будет людей: "))
 
 if target == "msc":
     target_price = msc_price
@@ -11,4 +12,4 @@ elif target == "spb":
 elif target == "ekb":
     target_price = ekb_price
 
-print(f"Стоимость поездки составит {target_price}. Приятного путешествия!")
+print(f"Стоимость поездки составит {count * target_price}. Приятного путешествия!")
